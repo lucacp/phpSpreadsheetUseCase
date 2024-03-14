0735f739d1f8ee9c593d2564a00df881cfa6031b
lucas6parnoff <lucas_parnoff@hotmail.com>
2024-03-14 02:34:06 -0300

    iniciando class para ler da planilha

--- Spreadsheet.php
+++ Spreadsheet.php
@@ -6,11 +6,12 @@ use PhpOffice\PhpSpreadsheet\Reader;
 
 class Spreadsheet
 {
-	protected function save($fileName){}
-	protected function load($fileName){}
-}
+	private $spreadsheet;
+	//protected function save($fileName){}
+	//protected function load($fileName){}
+};
 
-class XlsxSpreadsheet extends Spreedsheet
+class XlsxSpreadsheet extends Spreadsheet
 {
 	private $xlsxReader;
 	
@@ -18,9 +19,31 @@ class XlsxSpreadsheet extends Spreedsheet
 		$this->xlsxReader = new Reader\Xlsx();
 	}
 
-	protected function load($fileName){
+	function load($fileName){
 		$this->xlsxReader->setReadDataOnly(TRUE);
 		$loader = $this->xlsxReader->load($fileName);
-
+		$this->spreadsheet = $loader->getActiveSheet();
 	}
-}
+	function readActiveSheet(){
+		$colunas = range("A","M");
+		//print_r($colunas);
+		$response = array();
+		for($line = 1; $line < 2000; $line++){
+			$linha = '';
+			for($col = 0; $col < count($colunas);$col++){
+				$retorno = $this->spreadsheet->getCell(''.$colunas[$col].$line);
+				$linha.= strlen($linha)>2?'|'.$linha:'';
+			}
+			if(strlen($linha)>13){
+				array_push($response,$linha);
+			}
+		}
+		return $response;
+	}
+};
+
+$teste = new XlsxSpreadsheet();
+$teste->load("Lista Costa.xlsx");
+$response = $teste->readActiveSheet();
+print_r($response);
+unset($teste);
--- conexao.php
+++ conexao.php
@@ -1,5 +1,17 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
+use PDO;
+
 $caminho = __DIR__.'/banco.sqlite';
 $pdo = new PDO('sqlite:'.$caminho);
 echo 'Conectei';
+
+Class DAO
+{
+	private $pdo;
+	function getPdoConnection(PDO $connection){
+		$this->pdo = $connection;
+	}
+};
